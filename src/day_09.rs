4a6f3a87a02c35a74b4cb0eb8e296e90f70b4127
Charles Pierce <chuck@charlespierce.dev>
2023-12-09 11:06:58 -0800

    Day 9 Solutions

--- src/day_09.rs
+++ src/day_09.rs
@@ -1,11 +1,63 @@
 use aoc_runner_derive::aoc;
 
 #[aoc(day9, part1)]
-fn solve_part1(input: &str) -> usize {
-    0
+fn solve_part1(input: &str) -> isize {
+    input
+        .lines()
+        .map(|line| {
+            let sequence: Vec<_> = line.split(' ').map(|n| n.parse().unwrap()).collect();
+            next_value(&sequence)
+        })
+        .sum()
 }
 
 #[aoc(day9, part2)]
-fn solve_part2(input: &str) -> usize {
-    0
+fn solve_part2(input: &str) -> isize {
+    input
+        .lines()
+        .map(|line| {
+            let sequence: Vec<_> = line.split(' ').map(|n| n.parse().unwrap()).collect();
+            previous_value(&sequence)
+        })
+        .sum()
+}
+
+fn next_value(sequence: &[isize]) -> isize {
+    if sequence.len() <= 1 {
+        return 0;
+    }
+
+    let mut diffs = Vec::with_capacity(sequence.len() - 1);
+    for i in 1..sequence.len() {
+        diffs.push(sequence[i] - sequence[i - 1]);
+    }
+
+    let last = *sequence.last().unwrap();
+    let diff = if diffs.iter().all(|n| *n == 0) {
+        0
+    } else {
+        next_value(&diffs)
+    };
+
+    last + diff
+}
+
+fn previous_value(sequence: &[isize]) -> isize {
+    if sequence.len() <= 1 {
+        return 0;
+    }
+
+    let mut diffs = Vec::with_capacity(sequence.len() - 1);
+    for i in 1..sequence.len() {
+        diffs.push(sequence[i] - sequence[i - 1]);
+    }
+
+    let first = sequence[0];
+    let diff = if diffs.iter().all(|n| *n == 0) {
+        0
+    } else {
+        previous_value(&diffs)
+    };
+
+    first - diff
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@
 // pub mod day_05;
 // pub mod day_06;
 // pub mod day_07;
-pub mod day_08;
-// pub mod day_09;
+// pub mod day_08;
+pub mod day_09;
 // pub mod day_10;
 // pub mod day_11;
 // pub mod day_12;
